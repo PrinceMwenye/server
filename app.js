8e3785158bf3516d335b09c77cc5d8ee1d100092
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-10-31 23:48:19 -0700

    update CORS

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const definitionsRoute = require('./routes/definitions');
+const definitionsRoute = require('./routes/definition');
 const app = express();
 const port = process.env.PORT || 3000; // You can set a custom port or use 3000
-
+const cors = require('cors');
 app.use(bodyParser.json());
 
 app.use('/api/v1/definition', definitionsRoute);
+// Configure CORS to allow requests from 'http://localhost:3001'
+app.use(cors({
+    origin: 'http://localhost:3001', // Change to match the origin of your front-end
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // You can include this if needed
+}));
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
--- routes/definition.js
+++ routes/definition.js
@@ -5,28 +5,34 @@ const router = express.Router();
 // Import database functions and error handling logic here
 
 router.post('/', (req, res) => {
-  // Handle POST request to create a new dictionary entry
-  // Implement validation, insertion, and error handling here
+    console.log("Lets add")
+    // Access the word from the request body
+    const word = req.body.word;
+
+    // Handle POST request to create a new dictionary entry
+    // Implement validation, insertion, and error handling here
+
+    console.log("Adding definition for word:", word);
 });
 
 router.patch('/:word', (req, res) => {
-  // Handle PATCH request to update the definition of an existing word
-  // Implement validation, update, and error handling here
+    // Handle PATCH request to update the definition of an existing word
+    // Implement validation, update, and error handling here
 });
 
 router.get('/:word', (req, res) => {
-  // Handle GET request to retrieve the definition of a word
-  // Implement database query, error handling, and response here
+    // Handle GET request to retrieve the definition of a word
+    // Implement database query, error handling, and response here
 });
 
 router.delete('/:word', (req, res) => {
-  // Handle DELETE request to remove a word and its definition
-  // Implement database deletion, error handling, and response here
+    // Handle DELETE request to remove a word and its definition
+    // Implement database deletion, error handling, and response here
 });
 
 router.get('/languages', (req, res) => {
-  // Handle GET request to retrieve the list of languages
-  // Implement querying the 'language' table, error handling, and response here
+    // Handle GET request to retrieve the list of languages
+    // Implement querying the 'language' table, error handling, and response here
 });
 
 module.exports = router;
