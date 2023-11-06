d95c63969dc58575530aa7256ef42ba8b3691b8a
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-11-05 20:26:47 -0800

    debug cors

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+// const express = require('express');
+// require("dotenv").config();
+// const bodyParser = require('body-parser');
+// const definitionsRoute = require('./routes/definition');
+// const app = express();
+// const port = process.env.PORT || 3000;
+// const cors = require('cors');
+// app.use(bodyParser.json());
+
+
+
+// app.use(cors({
+//     origin: 'https://clientlab6-dd9cafc8770e.herokuapp.com',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+// }));
+
+// app.use('/api/v1/definition', definitionsRoute);
+
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
 const express = require('express');
 require("dotenv").config();
 const bodyParser = require('body-parser');
@@ -5,9 +29,8 @@ const definitionsRoute = require('./routes/definition');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
+
 app.use(bodyParser.json());
-
-
 
 app.use(cors({
     origin: 'https://clientlab6-dd9cafc8770e.herokuapp.com',
@@ -17,6 +40,13 @@ app.use(cors({
 
 app.use('/api/v1/definition', definitionsRoute);
 
+// Set the CORS headers for all routes
+app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://clientlab6-dd9cafc8770e.herokuapp.com");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
