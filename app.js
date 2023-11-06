ce7c11750967e2c0e4a3e1e06a948ad3be38c1fc
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-11-05 20:24:31 -0800

    test

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ require("dotenv").config();
 const bodyParser = require('body-parser');
 const definitionsRoute = require('./routes/definition');
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(bodyParser.json());
+
+app.use('/api/v1/definition', definitionsRoute);
+
 
 app.use(cors({
     origin: 'https://clientlab6-dd9cafc8770e.herokuapp.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
+    credentials: true,
 }));
 
 
-app.use('/api/v1/definition', definitionsRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
